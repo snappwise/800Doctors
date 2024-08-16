a9cf2fe4acfeb9857694b45be1e5d8fdcd4abed0
Om <omthakkar2021.comp@mmcoe.edu.in>
2024-08-16 14:29:01 +0530

    Added gallery

---
+++ static/scripts/gallery.js
@@ -0,0 +1,23 @@
+$(document).ready(function () {
+    $('#category1').lightGallery({
+        selector: 'a',
+        counter: true,
+        download: false,
+        zoom: true,
+        share: false,
+        rotate: false,
+        flip: false
+    });
+});
+
+$(document).ready(function () {
+    $('#category2').lightGallery({
+        selector: 'a',
+        counter: true,
+        download: false,
+        zoom: true,
+        share: false,
+        rotate: false,
+        flip: false
+    });
+});
