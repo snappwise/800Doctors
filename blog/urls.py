e1b71ba1fc472ee8bc385fdec767a23f54065500
VISHALYADAV112 <vishalyadavdev108@gmial.com>
2024-09-13 23:23:05 +0530

    Commit before pull

---
+++ blog/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from .views import BlogListView,BlogPageView,BlogDetailView
+
+urlpatterns = [
+    path('api/blogs/', BlogListView.as_view(), name='blog-list-api'),
+    path('blogs/', BlogPageView.as_view(), name='blog-page'),
+    path('blogs/<slug:slug>/', BlogDetailView.as_view(), name='blog-detail'),
+]
---
+++ blog/views.py
@@ -0,0 +1,57 @@
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from blog.models import Blog
+from blog.serializers import BlogSerializer
+from django.views.generic import TemplateView, DetailView
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+
+
+class BlogListView(APIView):
+    def get(self, request, *args, **kwargs):
+        blogs = Blog.objects.all()
+
+        current_page = request.GET.get("page", 1)
+
+        paginator = Paginator(blogs, 9)
+
+        try:
+            blogs_page = paginator.page(current_page)
+        except PageNotAnInteger:
+            blogs_page = paginator.page(1)
+        except EmptyPage:
+            blogs_page = paginator.page(paginator.num_pages)
+
+        serializer = BlogSerializer(blogs_page, many=True)
+
+        pagination_data = {
+            "current_page": int(current_page),
+            "total_pages": paginator.num_pages,
+            "has_next": blogs_page.has_next(),
+            "has_previous": blogs_page.has_previous(),
+            "next_page_number": (
+                blogs_page.next_page_number() if blogs_page.has_next() else None
+            ),
+            "previous_page_number": (
+                blogs_page.previous_page_number() if blogs_page.has_previous() else None
+            ),
+        }
+
+        return Response(
+            {"blogs": serializer.data, "pagination": pagination_data},
+            status=status.HTTP_200_OK,
+        )
+
+
+class BlogPageView(TemplateView):
+    template_name = "blogs-page.html"
+
+
+class BlogDetailView(DetailView):
+    model = Blog
+    template_name = "blog.html"
+    context_object_name = "blog"
+
+    # Override the method to use the slug field
+    def get_object(self):
+        return Blog.objects.get(blog_seo_title=self.kwargs["slug"])
